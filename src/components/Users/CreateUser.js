42b359a2525537785910c41a1b5083ea7a473596
parom <E.Parominskiy@isida.by>
2023-06-12 08:42:50 +0300

    imbedded Wrapper in React

--- src/components/Users/CreateUser.js
+++ src/components/Users/CreateUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
@@ -45,7 +45,7 @@ const CreateUser = (props) => {
   };
 
   return (
-    <React.Fragment>
+    <Fragment>
       {error && (
         <ErrorModal
           onCloseModal={errorHandler}
@@ -72,7 +72,7 @@ const CreateUser = (props) => {
           <Button type="submit">Добавить Пользователя</Button>
         </form>
       </Card>
-    </React.Fragment>
+    </Fragment>
   );
 };
 
